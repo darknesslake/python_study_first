7a93c76550ea759b33d22b005311dcd2430e2eaa
darknesslake <vabcax@gmail.com>
2024-09-06 13:00:59 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import telebot
+from telebot import types
+import telegram_token as tg_token
+
+# Замените 'YOUR_BOT_TOKEN' на ваш токен, полученный от BotFather
+bot = tg_token.bot
+
+# Приветствие для новых участников
+@bot.message_handler(content_types=['new_chat_members'])
+def greet_new_member(message):
+    for new_member in message.new_chat_members:
+        bot.reply_to(message, f"Привет, {new_member.first_name}! Добро пожаловать в нашу группу!")
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я бот, который приветствует новых участников. \n"
+                          "Если вы только что присоединились к группе, я уже вас поприветствовал. \n"
+                          "Если вам нужна помощь, обратитесь к администраторам группы.")
+
+
+
+# Запуск бота
+bot.polling(none_stop=True)
